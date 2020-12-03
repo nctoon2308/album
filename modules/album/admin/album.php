6ee6ebab16859e3fe8a18f6304dca265f379406b
nctoon2308 <namda1512016@gmail.com>
2020-12-03 15:59:28 +0700

    tạo album

--- modules/album/admin/album.php
+++ modules/album/admin/album.php
@@ -14,6 +14,67 @@ if (!defined('NV_IS_FILE_ADMIN')) {
 
 $page_title = $lang_module['main'];
 
+$post = [];
+$error = [];
+$upload_info =[];
+
+//xu ly anh
+if ($nv_Request->isset_request('submit', 'post') and isset($_FILES, $_FILES['image_album'], $_FILES['image_album']['tmp_name']) and is_uploaded_file($_FILES['image_album']['tmp_name'])) {
+    //
+    $upload = new NukeViet\Files\Upload($admin_info['allow_files_type'], $global_config['forbid_extensions'], $global_config['forbid_mimes'], NV_UPLOAD_MAX_FILESIZE, NV_MAX_WIDTH, NV_MAX_HEIGHT);
+
+    $upload->setLanguage($lang_global);
+
+    $upload_info = $upload->save_file($_FILES['image_album'], NV_UPLOADS_REAL_DIR.'/'.$module_name, false,$global_config['nv_auto_resize']);
+
+   /* die($upload_info['basename']);*/
+}
+
+$post['id'] = $nv_Request->get_int('id','post,get','0');
+
+$post['name_album'] = $nv_Request->get_title('name_album','post','');
+$post['active_album'] = $nv_Request->get_int('active_album','post','1');
+$post['desc_album'] = $nv_Request->get_title('desc_album','post','');
+$post['image_album'] = $upload_info['basename'];
+
+$post['submit'] = $nv_Request->get_title('submit','post');
+
+/*echo "<pre>";
+print_r($post);
+echo "</pre>";*/
+if (!empty($post['submit'])) {
+    if (empty($post['name_album'])) {
+        $error[] = "Chưa nhập tên";
+    }
+    if (empty($post['desc_album'])) {
+        $error[] = "Chưa nhập mô tả";
+    }
+
+    if (empty($error)) {
+        if ($post['id'] > 0) {
+            //UPDATE
+        } else {
+            $sql = "INSERT INTO `nv4_md_album`(`id`, `name_album`, `image_album`, `desc_album`, `active_album`, `create_at`) 
+            VALUES (NULL,:name_album,:image_album,:desc_album,:active_album, :create_at)";
+            $s = $db->prepare($sql);
+            $s->bindValue('create_at', NV_CURRENTTIME);
+        }
+        $s->bindParam('name_album', $post['name_album']);
+        $s->bindParam('active_album', $post['active_album']);
+        $s->bindParam('desc_album', $post['desc_album']);
+        $s->bindParam('image_album', $post['image_album']);
+
+        $exe = $s->execute();
+
+        if ($exe) {
+            $error[] = "OK";
+        } else {
+            $error[] = "Khong insert dc";
+        }
+    }
+
+}
+
 //------------------------------
 // Viết code xử lý chung vào đây
 //------------------------------
@@ -27,6 +88,11 @@ $xtpl->assign('NV_NAME_VARIABLE', NV_NAME_VARIABLE);
 $xtpl->assign('NV_OP_VARIABLE', NV_OP_VARIABLE);
 $xtpl->assign('MODULE_NAME', $module_name);
 $xtpl->assign('OP', $op);
+$xtpl->assign('POST', $post);
+$xtpl->assign('ERROR',implode('<br>',$error));
+if (!empty($error)){
+    $xtpl->parse('main.error');
+}
 
 //-------------------------------
 // Viết code xuất ra site vào đây
--- themes/admin_default/modules/album/album.tpl
+++ themes/admin_default/modules/album/album.tpl
@@ -1,5 +1,31 @@
 <!-- BEGIN: main -->
-<form action="{NV_BASE_ADMINURL}index.php?{NV_LANG_VARIABLE}={NV_LANG_DATA}&amp;{NV_NAME_VARIABLE}={MODULE_NAME}&amp;{NV_OP_VARIABLE}={OP}" method="post">
-    <div class="text-center"><input class="btn btn-primary" name="submit" type="submit" value="{LANG.save}" /></div>
+<!-- BEGIN: error -->
+<div class="alert alert-warning" role="alert">{ERROR}</div>
+<!-- END: error -->
+<form  enctype="multipart/form-data" action="{NV_BASE_ADMINURL}index.php?{NV_LANG_VARIABLE}={NV_LANG_DATA}&amp;{NV_NAME_VARIABLE}={MODULE_NAME}&amp;{NV_OP_VARIABLE}={OP}" method="post" name="{MODULE_NAME}">
+    <div class="form-group">
+        <h1><b>Tạo mới album</b></h1>
+        <input type="hidden" name="id" value="">
+        <div class="form-group">
+            <label for="name_album">Tên album:</label>
+            <input type="text" name="name_album" class="form-control" id="name_album" value="">
+        </div>
+        <div class="form-group">
+            <label for="active-album">Trạng thái album:</label>
+            <select name="active_album" id="">
+                <option value="1">Hiển thị</option>
+                <option value="2">Ẩn</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="image_album">Ảnh album:</label>
+            <input type="file" class="form-control" name="image_album" id="image_album" value="">
+        </div>
+        <div class="form-group">
+            <label for="desc_album">Mô tả album:</label>
+            <textarea name="desc_album" class="form-control" id="desc_album"  rows="10"></textarea>
+        </div>
+        <div class="text-center"><input class="btn btn-primary" name="submit" type="submit" value="{LANG.save}" /></div>
+    </div>
 </form>
 <!-- END: main -->
